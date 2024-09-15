93175d002f96ab11bac63ba573d9ddb9b4e14d6f
WERTYUIOKL <147541259+WERTYUIOKL@users.noreply.github.com>
2024-09-15 12:22:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import NewPost from './NewPost';
+import Selection from './Selection';
+import 'semantic-ui-css/semantic.min.css';
+
+
+
+function App() { 
+  return (
+    
+    <div className="App">
+      <NewPost/>
+      <Selection />     
+    </div>
+  );
+}
+export default App;
